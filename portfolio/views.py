a4413ed4765f940f6b4b3f0f42c046b99ff0b671
margaret254 <maggiemwas91@gmail.com>
2019-12-14 09:44:09 +0300

    Create gallery and portfolio folders and all relevant files

---
+++ portfolio/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime as dt
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome My Personal Gallery Application')
+
+def photo_category(request):
+    date = dt.date.today()
+    return HttpResponse
+
+def convert_dates(dates):
+    
