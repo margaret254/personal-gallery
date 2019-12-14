d89f078a8cba845955669a1fd9a57394ed9b2800
margaret254 <maggiemwas91@gmail.com>
2019-12-14 12:38:43 +0300

    Style base.html file using jumbotron and cards

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.photo_category,name='photoCategories')
+    url(r'^$',views.photo_category,name='photoCategory')
 ]
