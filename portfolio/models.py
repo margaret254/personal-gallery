9d2dde09e7cf78a2f10a188fbe2fef45cf93058f
margaret254 <maggiemwas91@gmail.com>
2019-12-15 15:03:11 +0300

    Create owner,category and location models plus make migrations

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,28 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+
 
 from django.db import models
 
 # Create your models here.
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+class category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+class location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
