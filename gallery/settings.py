1da899bbeabf44c56c86a80761b8bb4668d4c52b
margaret254 <maggiemwas91@gmail.com>
2019-12-15 18:29:21 +0300

    Add image models

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gallery',
+        'NAME': 'portfolio',
         'USER': 'moringa',
     'PASSWORD':'maggie',
     }
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,28 +1,27 @@
 
 
 from django.db import models
-
+import datetime as dt
 # Create your models here.
 
-class Owner(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.first_name
-
-    class Meta:
-        ordering = ['first_name']
-
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.name
 
-class location(models.Model):
+class Location(models.Model):
     name = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    title = models.CharField(max_length = 30)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    posted_date= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
