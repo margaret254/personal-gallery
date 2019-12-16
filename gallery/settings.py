972fadf2fb14cff0898b9c84319b15f059217edd
margaret254 <maggiemwas91@gmail.com>
2019-12-16 14:51:48 +0300

    Fix errors

--- gallery/settings.py
+++ gallery/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio',
         'USER': 'moringa',
-    'PASSWORD':'maggie',
+        'PASSWORD':'moringa254',
     }
 }
 
@@ -126,5 +127,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,10 +1,11 @@
-
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
 
 class ImageTestClass(TestCase):
     def setUp(self):
+        pass
 
         # Creating a new category
         self.new_category = Category(name='travel')
@@ -16,12 +17,32 @@ class ImageTestClass(TestCase):
 
         # Creating new Image
         self.new_image = Image(title='Image1',
-        description='Thisis my first image to upload')
+        description='This is my first image to upload')
         self.new_image.save()
 
     def tearDown(self):
         Category.objects.all().delete()
         Location.objects.all().delete()
         Image.objects.all().delete()
+
+        
+
+class LocationTestClass(TestCase):
+
+    """
+    A test that checks for the behaviour of the image class
+    """
+
+    def setUp(self):
+        pass
+
+class CategoryTestClass(TestCase):
+    """
+    A test  that checks the functionality of the image class
+    """
+
+    def setUp(self):
+        pass
+
 
         
