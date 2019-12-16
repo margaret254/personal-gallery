715df4f6be5a8b28349ed0a73f3781651deba2e4
margaret254 <maggiemwas91@gmail.com>
2019-12-16 14:48:42 +0300

    Add search results url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns=[
     url(r'^$',views.photo_category,name='photoCategory'),
-    url('^travel/$',views.travel,name='travel'),
-    url('^school/$',views.school,name='school'),
-    url('^party/$',views.party,name='party'),
-    url('^hiking/$',views.hiking,name='hiking'),
-    url('^nature/$',views.nature,name='nature'),
-    url('^family/$',views.family,name='family')
+    url(r'^search/', views.search_results, name='search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
